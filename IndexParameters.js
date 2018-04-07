f15ff003c0af263e5fc2ba89657cad48b3a70810
sterndragon <37169336+sterndragon@users.noreply.github.com>
2018-04-07 08:34:57 +0300

    Create  IndexParameters.js

---
+++ IndexParameters.js
@@ -0,0 +1,56 @@
+var numposts = 999;
+var showpostthumbnails = true;
+var indexWidth = 4;
+var thumbWidth = "120px";
+var thumbHeight = "90px";
+var titleWidth = "120px";
+var titleHeight = "90px";
+var cellPad = 8;
+var tableWidth = 90;
+var cellWidth = 120;
+var cellHeight = 90;
+  
+/* 
+  The parameters listed below control the look and feel of your thumbnail index page.
+  
+  numposts
+      This value controlls the maximum number of posts that will be fetched from your
+      Blogger rss feed. 
+      *Value 1 to 999.
+      
+  showpostthumbnails
+      If you set this to false you will not see thumbnails in your index. 
+      Mostly used for troubleshooting. 
+      *Value true or false
+      
+  indexWidth
+      This is the number of thumbnails that you want displayed in a single line.
+      
+  thumbWidth
+      This is the width of the thumbnail that the script will generate from your 
+      rss feed.
+      
+  thumbHeight
+      This is the height of the thumbnail that the script will generate from your 
+      rss feed.
+      
+  titleWidth
+      This is the width of the title block that will be displayed below the thumbnail
+      image.
+      
+  titleHeight
+      This is the height of the title block that will be displayed below the thumbnail
+      image.
+  
+  cellPad
+      This is the number of pixels between the cells in the table.
+      
+  tableWidth
+      This is the width of the table as a percentage of the page.
+      
+  cellWidth
+      This is the width of the cell that the script will use to display your thumbnail.
+      
+  cellHeight
+      This is the height of the cell that the script will use to display your thumbnail.    
+*/
